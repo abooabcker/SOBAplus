f7d2b8ff80e9b702e82203ab44069d97b5110a24
abooabcker <abukilayil@gmail.com>
2016-08-15 15:25:05 +0530

    shortend sobaplus
    avoid the pretty table,colors,unwanted lines
    avoid input stream
    changed in to a method

--- sobaplus.py
+++ sobaplus.py
@@ -1,28 +1,17 @@
-from prettytable import PrettyTable
-t=PrettyTable(['Particulars','Quantity','Rate'])
-amount=int(input("\033[30m"+" Enter the amount :\033[1;31m "))
-a=amount
-bal=0
-rs=[1000,500,100,50,20,10,5,2,1]
-clst=[]
-print ("\033[30m howmany need in\033[1;31m ")
-for i in range(9):
-   n= (input("{}  :  ".format(rs[i])))
-   clst.append(n)
-print("\033[1;32m")
-for i in  range (9):
-    j=clst[i]
-    while j >0:
-        tot=j*rs[i]
-        if tot < a+1:
-           t.add_row([rs[i],j,tot])
-           bal=bal+tot
-           a=a-tot
-           j=0
-        j=j-1
-print t
-print(" Total : {}".format(bal).rjust(30))
-print ("\033[31m Balance : {}".format(amount-bal).rjust(40))
+def sobaplus(amount,list):
+   result={}
+   rs=[1000,500,100,50,20,10,5,2,1]
+   for i in  rs:
+       if list.get(i,0) > 0:
+          for list[i] in range(list[i],0,-1):
+             if list[i]*i <= amount:
+                result[i]=list[i]
+                amount=amount-(i*list[i])
+                break
+       if amount <=0:
+                 break
+   return ( result,amount )
+
         
         
     
